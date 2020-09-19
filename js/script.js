1e835b8fbb8d8b583f280208312ca96986fb9b47
Jelena Feliciano <68608110+Jaaylena@users.noreply.github.com>
2020-09-19 14:14:01 -0400

    Update script.js
    appendPageLinks() function has the numbers appearing to the page but when clicked nothing happens

--- js/script.js
+++ js/script.js
@@ -5,24 +5,11 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-//
 document.addEventListener('DOMContentLoaded', () => {
    const studentList = document.querySelectorAll('.student-item');
    const perPage = 10;
    const divClassPage = document.querySelector('.page');
-   const li = document.querySelectorAll('.student-list');
-   
+
    /***function to display 10 students to a page at a time. ***/
        function showPage(studentList, page) { 
          const firstIndex = page * 10 - 10;
@@ -40,40 +27,34 @@ document.addEventListener('DOMContentLoaded', () => {
           
          }
       }
-      /**testing above function console.log(showPage(studentList, 1 ));
-       * to be deleted */
    
-     /**creating the amount of pages needed to display all 54 students
-      * at 10 students a page */ 
+     /**a funtion that appends the list of 10 students to each page when
+      * the page number is  */ 
       function appendPageLinks(list) {
          //calculating the number of pages needed to display the student list
          const numberOfPgs = Math.ceil(list.length / 10);
-         // creating a div with pagination class and appending it to the div.page
+         /* creating a div with pagination class with a ul that stores the pg links
+          and appending it to the div.page**/
          const divPagination = document.createElement('div');
          const ulPagination = document.createElement('ul');
             divPagination.className = 'pagination';
             divClassPage.appendChild(divPagination);
-         divPagination.appendChild(ulPagination);
-         //adding ul to the pagination div that stores the links
-         //a linked page number for each page
+            divPagination.appendChild(ulPagination);
+          
+     
+         //a linked page number for each page needed to display each student
       
          for(let i = 0; i < numberOfPgs; i++) {
             const pgLink = document.createElement('a');
             const paginationLi = document.createElement('li');
-               pgLink.textContent = (i + 1);
-               paginationLi.appendChild(pgLink);
-               ulPagination.appendChild(paginationLi);
-
+            pgLink.href = '#';
+            pgLink.textContent = (i + 1).toString();
+            paginationLi.appendChild(pgLink);
+            ulPagination.appendChild(paginationLi);
          }
-        return `
-         <div class="pagination">
-            <ul>
-         
-         </div> 
-        `
-
+       
       }
-   
+      appendPageLinks(studentList);
       showPage(studentList, 1 );
       
    });
