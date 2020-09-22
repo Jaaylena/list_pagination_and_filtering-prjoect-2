8fafb3701672d985328b93ce13853277f33fb4df
Jelena Feliciano <68608110+Jaaylena@users.noreply.github.com>
2020-09-22 00:41:06 -0400

    Update script.js
    created the search bar.

--- js/script.js
+++ js/script.js
@@ -8,56 +8,61 @@ FSJS project 2 - List Filter and Pagination
 document.addEventListener('DOMContentLoaded', () => {
    const studentList = document.querySelectorAll('.student-item');
    const perPage = 10;
-  
-   //function for searching names
-   function nameSearch(searchInput, names) {
-      //creating the search bar 
-      const h2 = document.getElementById('h2');
-      const searchDiv = document.createElement('div');
-         searchDiv.className = 'search-bar';
-         h2.appendChild(searchDiv);
-      const form = document.createElement('form');
-         searchDiv.appendChild(form);
-//filtering the names 
-         for(let n = 0; n < names.length; n++){
-         name[n].classList.remove('active');
-         if(searchInput.value.length !== 0 && names[i].toLowerCase().includes(searchInput.value.toLowerCase())){
-            names[i].classList.add('active');
-         }
-      }
-      console.log(searchInput);
-      console.log(names);
+//search function creates an search box 
+function nameSearch(input, names) {
+   const pageHeaderDiv = document.querySelector('.page-header');
+  //creating a searchbar and appending it to the header 
+   const headerDiv = document.createElement('div');
+      headerDiv.className = 'student-search';
+      pageHeaderDiv.appendChild(headerDiv);
+   const searchInput = document.createElement('input');
+   headerDiv.appendChild(searchInput);
+   searchInput.className = 'student';
+   searchInput.type = 'text';
+   searchInput.placeholder = 'Search for Students...'; 
+//creating a search button and appending it to the searchbar
+   const searchButton = document.createElement('button');
+   headerDiv.appendChild(searchButton);
+   searchButton.type = 'submit';
+   searchButton.textContent = 'Submit';
 
+   for(let n = 0; n < names.length; n++){
+      names[n].classList.remove('active');
+      //if(Input.value.length !== 0 && names[n].toLowerCase().includes(Input.value.toLowerCase())){
+      //   names[n].classList.add('active');
+      //}
+   console.log(names);
    }
-
+ }
+ nameSearch();
    /***function to display 10 students to a page at a time. ***/
-       function showPage(studentList, page) { 
-         const firstIndex = page * 10 - 10;
-         const lastIndex = page * 10 - 1;
+function showPage(studentList, page) { 
+   const firstIndex = page * 10 - 10;
+   const lastIndex = page * 10 - 1;
          
-         // Loop over items in the list parameter creating the list of ten students to a page
-         for(let i = 0; i < studentList.length; i++) {
-            if ( i >= firstIndex && i <= lastIndex) {
-               //to show items if true
-               studentList[i].style.display = 'block';
-            } else {
-               //to hide items if false
-               studentList[i].style.display = 'none';
-            }
+ // Loop over items in the list parameter creating the list of ten students to a page
+    for(let i = 0; i < studentList.length; i++) {
+    if ( i >= firstIndex && i <= lastIndex) {
+ //to show items if true
+     studentList[i].style.display = 'block';
+    } else {
+   //to hide items if false
+      studentList[i].style.display = 'none';
+       }
           
-         }
       }
+   }
    
-     /**a funtion that appends the list of 10 students to each page when
-      * the page number is  */ 
+     /**a funtion that determines the number of pages needed to append 10 out of 54
+      * students */ 
       function appendPageLinks(list) {
          //calculating the number of pages needed to display the student list
-         const numberOfPgs = Math.ceil(list.length / perPage);
+         const numberOfPgs = Math.ceil(list.length / 10);
          /* creating a div with pagination class with a ul that stores the pg links
           and appending it to the div.page**/
+          const divClassPage = document.querySelector('.page');
          const divPagination = document.createElement('div');
          const ulPagination = document.createElement('ul');
-          const divClassPage = document.querySelector('.page');
             divPagination.className = 'pagination';
             divClassPage.appendChild(divPagination);
             divPagination.appendChild(ulPagination);
@@ -66,29 +71,28 @@ document.addEventListener('DOMContentLoaded', () => {
          for(let i = 0; i < numberOfPgs; i++) {
             const pgLink = document.createElement('a');
             const paginationLi = document.createElement('li');
-            pgLink.href = '#';
             pgLink.textContent = `${i+1}`;
             paginationLi.appendChild(pgLink);
             ulPagination.appendChild(paginationLi);
+   //highlights the active page
             if(i === 0) {
                pgLink.classList.add('active');
             }else{
                pgLink.classList.remove('active');
             }
-//when page number links are clicked the appropriate list is displayed
-            pgLink.addEventListener('click', (e) => {
+//when a page number link is clicked the corresponding list is displayed
+            pgLink.addEventListener('click', (event) => {
                const pageNumbers = document.querySelectorAll('.pagination a');
                   showPage(studentList, i + 1);
                   for(let p = 0; p < pageNumbers.length; p++) {
                      pageNumbers[p].classList.remove('active');
-                     e.target.classList.add('active');
+                     event.target.classList.add('active');
                   }  
                   
                });
          }
    //changes the list of 10 student that appear according to the page selected
-        
-       
+   
       }
       showPage(studentList, 1);
       appendPageLinks(studentList);
